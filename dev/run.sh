f0f7320799281c3ac40e5a3b2c4e14a8fc65cdbc
MatteoDonati <matteo.donati_1@outlook.it>
2024-08-12 14:37:15 +0200

    fixed script for dev tests

--- dev/run.sh
+++ dev/run.sh
@@ -4,26 +4,26 @@
 PROGRAM_SRC="$1"
 PROGRAM_NAME=$(basename "$PROGRAM_SRC" .c)
 PROGRAM_DIR=$(dirname "$PROGRAM_SRC")
-BIN_DIR="$PROGRAM_DIR/bin/$PROGRAM_NAME"
+BASE_DIR=$(dirname "$PROGRAM_DIR")
+BIN_DIR="$BASE_DIR/bin/$PROGRAM_NAME"
 
 # Create the binary directory if it doesn't exist
-mkdir -p "$BIN_DIR"
+mkdir -p "$BIN_DIR" > /dev/null 2>&1
 
-# Uninstall the library
-sudo make uninstall
+# Uninstall the library silently
+sudo make uninstall > /dev/null 2>&1
 
-# Clean project directory
-make clean
+# Clean project directory silently
+make clean > /dev/null 2>&1
 
-# Compile the library
-make
+# Compile the library silently
+make > /dev/null 2>&1
 
-# Install the library
-sudo make install
+# Install the library silently
+sudo make install > /dev/null 2>&1
 
 # Compile the program and place the binary in the specified directory
-gcc -o "$BIN_DIR/$PROGRAM_NAME" "$PROGRAM_SRC" -lcortex -L/usr/local/lib -I/usr/local/include/cortex
+gcc -o "$BIN_DIR/$PROGRAM_NAME" "$PROGRAM_SRC" -lcortex -L/usr/local/lib -I/usr/local/include/cortex > /dev/null 2>&1
 
 # Run the program
-echo ""
 "$BIN_DIR/$PROGRAM_NAME"
